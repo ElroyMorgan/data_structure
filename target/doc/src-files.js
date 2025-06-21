5bc8052a4ee77dd511cee576d83e8bedd6446ea8
independence-code <zx8467@outlook.com>
2025-06-22 01:36:41 +0800

    Remove target directory

--- target/doc/src-files.js
+++
@@ -1,3 +0,0 @@
-var srcIndex = new Map(JSON.parse('[["data_structure",["",[["linear",[],["array.rs","list_node.rs","mod.rs","stack.rs","string.rs"]]],["lib.rs","sorting.rs"]]]]'));
-createSrcSidebar();
-//{"start":36,"fragment_lengths":[123]}
